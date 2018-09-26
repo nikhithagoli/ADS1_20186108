05e299bc8ec172ca37392176ecc10f747d3dee0a
nikhitha goli 20186105 <nikhithagolinikki86@msitprogram.net>
2018-09-26 17:57:31 +0530

    "check_style_score -> 0 of 3 passed. style score: 0/1 "

---
+++ assignments/m3/Sorted_Array/Solution.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class Solution{
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int m = sc.nextInt();
+		int n = sc.nextInt();
+		int[] first = new int[m];
+		int[] second = new int[n];
+		int k = 0;
+		for(String each : sc.nextLine().split(",")){
+			first[k++] = Integer.parseInt(each);
+		}
+		k = 0;
+		for(String each : sc.nextLine().split(",")){
+			second[k++] = Integer.parseInt(each);
+		}
+		int[] result = new int[m+n];
+		for(int i = 0, j = 0; i < m || j < n; i++,j++){
+			result[i] = first[i];
+			result[i+m] = second[j];
+		}
+		for(int i = 0; i<m+n; i++){
+			System.out.print(result[i] + ",");
+		}	
+	}
+}
