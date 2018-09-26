92879ff3774c8b9054d7ba5aecc5c94f80ddb3de
nikhitha goli 20186105 <nikhithagolinikki86@msitprogram.net>
2018-09-26 20:00:19 +0530

    "check_style_score -> 0 of 3 passed. style score: 0/1 "

--- assignments/m3/Sorted_Array/Solution.java
+++ assignments/m3/Sorted_Array/Solution.java
@@ -6,21 +6,13 @@ public class Solution{
 		int n = sc.nextInt();
 		int[] first = new int[m];
 		int[] second = new int[n];
-		/*int k = 0;
+		int k = 0;
 		for(String each : sc.nextLine().split(",")){
 			first[k++] = Integer.parseInt(each);
 		}
 		k = 0;
 		for(String each : sc.nextLine().split(",")){
 			second[k++] = Integer.parseInt(each);
-		}*/
-		for(int k = 0; k < m; k++){
-			first[k] = sc.nextInt();
-			sc.next();
-		}
-		for(int k = 0; k < m; k++){
-			second[k] = sc.nextInt();
-			sc.next();
 		}
 		int[] result = new int[m+n];
 		for(int i = 0, j = 0; i < m || j < n; i++,j++){
---
+++ practice/m3/BinarySearch/Solution.java
@@ -0,0 +1,56 @@
+import java.util.Scanner;
+import java.util.Arrays;
+public class Solution{
+	/*static int binarysearch(int[] arr, int first, int last, int search){
+		if(first<last){
+			int middle = (first + last)/2;
+			if(arr[middle] == search){
+				int k = middle;
+				while(arr[k] == arr[k-1]){
+					k--;
+				}
+				return k;
+			}
+			else if(search < arr[middle]){
+				return binarysearch(arr, first, middle-1, search);
+			}
+			else{
+				return binarysearch(arr, middle + 1, last, search);
+			}
+		}
+		return -1;
+	}*/
+	static int binarysearch(int[] arr, int first, int last, int search){
+		while(first < last){
+			int middle = (first + last)/2;
+			if(arr[middle] == search){
+				if(arr[middle] == search){
+					int k = middle;
+					while(arr[k] == arr[k-1]){
+						k--;
+					}
+					return k;
+				}
+			}
+			else if(search < arr[middle]){
+				last = middle - 1;
+			}
+			else{
+				first = middle+1;
+			}
+		}
+		return -1;
+	}
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int[] array = new int[n];
+		for(int i = 0; i < n; i++){
+			array[i] = sc.nextInt();
+		}
+		Arrays.sort(array);
+		int search = sc.nextInt();
+		System.out.println(binarysearch(array, 0, n, search));
+		
+	}
+}
