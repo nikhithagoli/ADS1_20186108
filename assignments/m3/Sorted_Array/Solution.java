987244ea03c94f15c37c6ecd74ad992c9096f5b9
nikhitha goli 20186105 <nikhithagolinikki86@msitprogram.net>
2018-09-26 21:14:23 +0530

    "check_style_score -> 3 of 3 passed. style score: 0/1 "

--- assignments/m3/Sorted_Array/Solution.java
+++ assignments/m3/Sorted_Array/Solution.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class Solution{
+public class Solution {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int m = sc.nextInt();
@@ -7,42 +7,42 @@ public class Solution{
 		sc.nextLine();
 		int[] first = new int[m];
 		int[] second = new int[n];
-		int k = 0, i =0, j = 0;
+		int k = 0, i = 0, j = 0;
 		String str = "";
 		String[] s;
-		if(m != 0){
+		if (m != 0) {
 			s = sc.nextLine().split(",");
-			for(String each : s){
+			for (String each : s) {
 				first[k++] = Integer.parseInt(each);
 			}
 		} else {
 			sc.nextLine();
 		}
 		k = 0;
-		if(n != 0){
+		if (n != 0) {
 			s = sc.nextLine().split(",");
-			for(String each : s){
+			for (String each : s) {
 				second[k++] = Integer.parseInt(each);
 			}
-		} else{
+		} else {
 			sc.nextLine();
 		}
-      	while (i < m && j < n) {
-        	if (first[i] < second[j])
-          		str += Integer.toString(first[i++]) + ",";
-        	else if (first[i] > second[j])
-          		str += Integer.toString(second[j++])+",";
-        	else {
-          		str += Integer.toString(second[j++])+",";
-          		i++;
-        	}
+		while (i < m && j < n) {
+			if (first[i] < second[j])
+				str += Integer.toString(first[i++]) + ",";
+			else if (first[i] > second[j])
+				str += Integer.toString(second[j++]) + ",";
+			else {
+				str += Integer.toString(second[j++]) + ",";
+				i++;
+			}
 		}
-		while(i < m){
+		while (i < m) {
 			str += Integer.toString(first[i++]) + ",";
 		}
-		while(j < n){
+		while (j < n) {
 			str += Integer.toString(second[j++]) + ",";
 		}
-		System.out.println(str.substring(0,str.length()-1));
+		System.out.println(str.substring(0, str.length() - 1));
 	}
 }
