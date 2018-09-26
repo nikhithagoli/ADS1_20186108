9b98ed96123b0ee01b86847fc1cb049989f8a428
nikhitha goli 20186105 <nikhithagolinikki86@msitprogram.net>
2018-09-26 17:26:51 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- assignments/m3/3-sum/Solution.java
+++ assignments/m3/3-sum/Solution.java
@@ -4,47 +4,50 @@ import java.util.Arrays;
  * Class for solution.
  */
 public class Solution {
-	/**
-	 * main.
-	 *
-	 * @param      args  The arguments
-	 */
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
-		int[] a = new int[n];
-		for (int i = 0; i < n; i++) {
-			a[i] = sc.nextInt();
-		}
-		int count = 0;
-		/*for(int i = 0; i < n; i++){
-			for(int j = i + 1; j < n; j++){
-				for(int k = j + 1; k < n; k++){
-					if(a[i]+a[j]+a[k] == 0){
-						count ++;
-					}
-				}
-			}
-		}*/
-		int j, k;
-		Arrays.sort(a);
-		//System.out.println(Arrays.toString(a));
-		for (int i = 0; i < n - 2; i++) {
-			j = i + 1;
-			k = n - 1;
-			//System.out.println(j+ "           " + k);
-			while (j < k) {
-				if (a[i] + a[j] + a[k] == 0) {
-					count ++;
-					j++;
-					k--;
-				} else if (a[i] + a[j] + a[k] < 0) {
-					j++;
-				} else {
-					k--;
-				}
-			}
-		}
-		System.out.println(count);
-	}
+    private Solution(){
+        //constructor.
+    }
+    /**
+     * main.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = sc.nextInt();
+        }
+        int count = 0;
+        /*for(int i = 0; i < n; i++){
+            for(int j = i + 1; j < n; j++){
+                for(int k = j + 1; k < n; k++){
+                    if(a[i]+a[j]+a[k] == 0){
+                        count ++;
+                    }
+                }
+            }
+        }*/
+        int j, k;
+        Arrays.sort(a);
+        //System.out.println(Arrays.toString(a));
+        for (int i = 0; i < n - 2; i++) {
+            j = i + 1;
+            k = n - 1;
+            //System.out.println(j+ "           " + k);
+            while (j < k) {
+                if (a[i] + a[j] + a[k] == 0) {
+                    count++;
+                    j++;
+                    k--;
+                } else if (a[i] + a[j] + a[k] < 0) {
+                    j++;
+                } else {
+                    k--;
+                }
+            }
+        }
+        System.out.println(count);
+    }
 }
