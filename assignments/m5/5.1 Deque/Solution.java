916c57601f7792d2f216ff28bf89558cab13ee9c
nikhitha goli 20186105 <nikhithagolinikki86@msitprogram.net>
2018-09-28 13:32:37 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- assignments/m5/5.1 Deque/Solution.java
+++ assignments/m5/5.1 Deque/Solution.java
@@ -1,20 +1,52 @@
 import java.util.Scanner;
+/**
+ * Class for deque.
+ */
 class Deque {
+    /**
+     * no of elements.
+     */
     int noOfElements;
+    /**
+     * first, last nodes.
+     */
     Node first, last;
+    /**
+     * Class for node.
+     */
     private class Node {
+        /**
+         * data.
+         */
         String data;
+        /**
+         * next link.
+         */
         Node next;
-        Node(String val, Node link) {
+        /**
+         * Constructs the object.
+         *
+         * @param      val   The value
+         * @param      link  The link
+         */
+        Node(final String val, final Node link) {
             this.data = val;
             this.next = link;
         }
     }
+    /**
+     * Constructs the object.
+     */
     Deque() {
         noOfElements = 0;
         first = null;
         last = null;
     }
+    /**
+     * Pushes a left.
+     *
+     * @param      value  The value
+     */
     void pushLeft(String value) {
         if (first == null) {
             first = new Node(value, null);
@@ -30,6 +62,11 @@ class Deque {
 
         noOfElements++;
     }
+    /**
+     * Pushes a right.
+     *
+     * @param      value  The value
+     */
     void pushRight(String value) {
         if (last == null) {
             last = new Node(value, null);
@@ -45,7 +82,9 @@ class Deque {
         }
         noOfElements++;
     }
-
+    /**
+     * popleft.
+     */
     void popLeft() {
         if (first != null) {
             Node popped = first;
@@ -55,7 +94,9 @@ class Deque {
             noOfElements--;
         }
     }
-
+    /**
+     * popright.
+     */
     void popRight() {
         if (last != null) {
             Node temp = null;
@@ -72,14 +113,27 @@ class Deque {
             noOfElements--;
         }
     }
+    /**
+     * size.
+     *
+     * @return     { description_of_the_return_value }
+     */
     int size() {
         return noOfElements;
     }
-
+    /**
+     * Determines if empty.
+     *
+     * @return     True if empty, False otherwise.
+     */
     boolean isEmpty() {
         return first == null;
     }
-
+    /**
+     * prints.
+     *
+     * @return     { description_of_the_return_value }
+     */
     String print() {
         if (noOfElements != 0) {
             String str = "";
@@ -95,7 +149,15 @@ class Deque {
     }
 
 }
+/**
+ * Class for solution.
+ */
 class Solution {
+    /**
+     * { function_description }
+     *
+     * @param      args  The arguments
+     */
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
