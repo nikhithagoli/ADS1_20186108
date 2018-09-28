99fa86f8fd73aca71cbb594473144f95914e23cd
nikhitha goli 20186105 <nikhithagolinikki86@msitprogram.net>
2018-09-28 13:38:21 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- assignments/m5/5.1 Deque/Solution.java
+++ assignments/m5/5.1 Deque/Solution.java
@@ -152,11 +152,11 @@ class Deque {
 /**
  * Class for solution.
  */
-class Solution {
+final class Solution {
     /**
      * Constructs the object.
      */
-    private Solution(){
+    private Solution() {
         //function.
     }
     /**
@@ -164,7 +164,7 @@ class Solution {
      *
      * @param      args  The arguments
      */
-    public static void main(String[] args) {
+    public static void main(final String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         sc.nextLine();
