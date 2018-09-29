fd31734ef998dfeb6b0d46ca297cbb69068b2fd8
nikhitha goli 20186105 <nikhithagolinikki86@msitprogram.net>
2018-09-29 11:45:52 +0530

    "check_style_score -> 6 of 12 passed. style score: 0/1 "

--- assignments/m6/Assignment-1/Solution.java
+++ assignments/m6/Assignment-1/Solution.java
@@ -21,7 +21,7 @@ import java.util.Stack;
 //         top = 0;
 //     }
 //     void resize(){
-//     	stack = Arrays.copyOf(stack, stack.length*2);
+//      stack = Arrays.copyOf(stack, stack.length*2);
 //     }
 //     /**
 //      * pushes.
@@ -29,9 +29,9 @@ import java.util.Stack;
 //      * @param      item  The item
 //      */
 //     void push(final String item) {
-//     	if(top == stack.length){
-//     		resize();
-//     	}
+//      if(top == stack.length){
+//          resize();
+//      }
 //         stack[top++] = item;
 //     }
 //     /**
@@ -70,101 +70,106 @@ import java.util.Stack;
 //     }
 
 // }
-class LinkedList{
-	class Node{
-		String digit;
-		Node next;
-	}
-	Node head, last,temp;
-	LinkedList(){
-		head =null;
-		last = null;
-	}
+class LinkedList {
+    class Node {
+        String digit;
+        Node next;
+    }
+    Node head, last, temp, newnode;
+    LinkedList() {
+        head = null;
+        last = null;
+    }
 
-	void insert(String val){
-		if(head == null){
-			head= new Node();
-			head.digit = val;
-			head.next = null;
-			last = head;
-			temp = head;
-		} else{
-			Node newnode = new Node();
-			newnode.digit = val;
-			newnode.next = null;
-			last.next = newnode;
-			last = newnode;
+    void insert(String val) {
+        if (head == null) {
+            head = new Node();
+            head.digit = val;
+            head.next = null;
+            last = head;
+            temp = head;
+        } else {
+            newnode = new Node();
+            newnode.digit = val;
+            newnode.next = null;
+            last.next = newnode;
+            last = newnode;
+            newnode = null;
 
-		}
-	}
-	void insertleft(String val){
-		if(head == null){
-			head = new Node();
-			head.digit = val;
-			head.next = null;
-		} else {
-			Node newnode = new Node();
-			newnode.digit = val;
-			newnode.next = head;
-			head = newnode;
-		}
-	}
+        }
+    }
+    void insertleft(String val) {
+        if (head == null) {
+            head = new Node();
+            head.digit = val;
+            head.next = null;
+        } else {
+            newnode = new Node();
+            newnode.digit = val;
+            newnode.next = head;
+            head = newnode;
+            newnode = null;
+        }
+    }
 }
 class AddLargeNumbers {
-    
+
     public static LinkedList numberToDigits(String number) {
-    	LinkedList l = new LinkedList();
-    	String[] digits = number.split("");
-    	for(String each: digits){
-    		l.insert(each);
-    	}
-    	return l;
+        LinkedList l = new LinkedList();
+        String[] digits = number.split("");
+        for (String each : digits) {
+            l.insert(each);
+        }
+        return l;
     }
 
     public static String digitsToNumber(LinkedList list) {
-    	String str = "";
-    	list.temp = list.head;
-    	while(list.temp != null){
-    		str += list.temp.digit;
-    		list.temp = list.temp.next;
-    	}
-    	return str;
+        String str = "";
+        list.temp = list.head;
+        while (list.temp != null) {
+            str += list.temp.digit;
+            list.temp = list.temp.next;
+        }
+        list.temp = null;
+        return str;
     }
 
     public static LinkedList addLargeNumbers(LinkedList list1, LinkedList list2) {
-    	LinkedList added = new LinkedList();
-    	// Stack stack1 = new Stack();
-    	// Stack stack2 = new Stack();
-    	Stack<String> stack1 = new Stack<>();
-    	Stack<String> stack2 = new Stack<>();
-      	list1.temp = list1.head;
-    	while(list1.temp != null){
-    		stack1.push(list1.temp.digit);
-    	}
-    	list2.temp = list2.head;
-    	while(list2.temp != null){
-    		stack2.push(list2.temp.digit);
-    	}
-    	int carry = 0;
-    	while(!stack1.isEmpty() && !stack2.isEmpty()){
-    		int a =Integer.parseInt(stack1.pop());
-    		int b =Integer.parseInt(stack2.pop());
-    		String[] res =Integer.toString(a+b+carry).split("");
-    		if(res.length == 2){
-    			added.insertleft(res[1]);
-    			carry = Integer.parseInt(res[0]);
-    		} else{
-    			added.insertleft(res[0]);
-    		}
-    		
-    	}
-    	while(!stack1.isEmpty()){
-    		added.insertleft(stack1.pop());
-    	}
-    	while(!stack2.isEmpty()){
-    		added.insertleft(stack2.pop());
-    	}
-    	return added;
+        LinkedList added = new LinkedList();
+        // Stack stack1 = new Stack();
+        // Stack stack2 = new Stack();
+        Stack<String> stack1 = new Stack<>();
+        Stack<String> stack2 = new Stack<>();
+        list1.temp = list1.head;
+        while (list1.temp != null) {
+            stack1.push(list1.temp.digit);
+        }
+        list1.temp = null;
+        list2.temp = list2.head;
+        while (list2.temp != null) {
+            stack2.push(list2.temp.digit);
+        }
+        list2.temp = null;
+        int carry = 0;
+        while (!stack1.isEmpty() && !stack2.isEmpty()) {
+            int a = Integer.parseInt(stack1.pop());
+            int b = Integer.parseInt(stack2.pop());
+            String[] res = Integer.toString(a + b + carry).split("");
+            if (res.length == 2) {
+                added.insertleft(res[1]);
+                carry = Integer.parseInt(res[0]);
+            } else {
+                added.insertleft(res[0]);
+            }
+
+        }
+        while (!stack1.isEmpty()) {
+            added.insertleft(stack1.pop());
+        }
+        while (!stack2.isEmpty()) {
+            added.insertleft(stack2.pop());
+        }
+        return added;
     }
 }
 
@@ -174,21 +179,21 @@ public class Solution {
         String input = sc.nextLine();
         String p = sc.nextLine();
         String q = sc.nextLine();
-        switch(input){
-            case "numberToDigits":
-                LinkedList pDigits = AddLargeNumbers.numberToDigits(p);
-                LinkedList qDigits = AddLargeNumbers.numberToDigits(q);
-                System.out.println(AddLargeNumbers.digitsToNumber(pDigits));
-                System.out.println(AddLargeNumbers.digitsToNumber(qDigits));
-                break;
+        switch (input) {
+        case "numberToDigits":
+            LinkedList pDigits = AddLargeNumbers.numberToDigits(p);
+            LinkedList qDigits = AddLargeNumbers.numberToDigits(q);
+            System.out.println(AddLargeNumbers.digitsToNumber(pDigits));
+            System.out.println(AddLargeNumbers.digitsToNumber(qDigits));
+            break;
 
-            case "addLargeNumbers":
-                pDigits = AddLargeNumbers.numberToDigits(p);
-                qDigits = AddLargeNumbers.numberToDigits(q);
-                LinkedList result = AddLargeNumbers.addLargeNumbers(pDigits, qDigits);
-                System.out.println(AddLargeNumbers.digitsToNumber(result));
-                break;
+        case "addLargeNumbers":
+            pDigits = AddLargeNumbers.numberToDigits(p);
+            qDigits = AddLargeNumbers.numberToDigits(q);
+            LinkedList result = AddLargeNumbers.addLargeNumbers(pDigits, qDigits);
+            System.out.println(AddLargeNumbers.digitsToNumber(result));
+            break;
         }
     }
-    
+
 }
