d5519326fcd0facea235dcb8dd315473fc71be1b
nikhitha goli 20186105 <nikhithagolinikki86@msitprogram.net>
2018-10-02 15:53:20 +0530

    "check_style_score -> 6 of 6 passed. style score: 0/1 "

--- assignments/m8/8.1 Team Ranking With Insertion/Solution.java
+++ assignments/m8/8.1 Team Ranking With Insertion/Solution.java
@@ -1,37 +1,94 @@
 import java.util.Scanner;
-import java.util.ArrayList;
+/**
+ * Class for details.
+ */
 class Details {
+    /**
+     * name.
+     */
     private String name;
+    /**
+     * no of wins.
+     */
     private int wins;
+    /**
+     * no of lost.
+     */
     private int lost;
+    /**
+     * no of draws.
+     */
     private int draws;
+    /**
+     * constuctor.
+     *
+     * @param      name   The name
+     * @param      wins   The wins
+     * @param      lost   The lost
+     * @param      draws  The draws
+     */
     Details(String name, int wins, int lost, int draws) {
         this.name = name;
         this.wins = wins;
         this.lost = lost;
         this.draws = draws;
     }
+    /**
+     * returns name.
+     *
+     * @return     { description_of_the_return_value }
+     */
     String getname() {
         return this.name;
     }
+    /**
+     * returns no of wins.
+     *
+     * @return     { description_of_the_return_value }
+     */
     int getwins() {
         return this.wins;
     }
+    /**
+     * returns no of lost.
+     *
+     * @return     { description_of_the_return_value }
+     */
     int getlost() {
         return this.lost;
     }
+    /**
+     * returns no of draws.
+     *
+     * @return     { description_of_the_return_value }
+     */
     int getdraws() {
         return this.draws;
     }
 }
+/**
+ * Class for solution.
+ */
 class Solution {
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
+        //function.
+    }
+    /**
+     * main.
+     *
+     * @param      args  The arguments
+     */
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         Details[] list = new Details[10];
         int n = 0;
         while (sc.hasNextLine()) {
             String[] tokens = sc.nextLine().split(",");
-            list[n++] = new Details(tokens[0], Integer.parseInt(tokens[1]), Integer.parseInt(tokens[2]), Integer.parseInt(tokens[3]));
+            list[n++] = new Details(tokens[0], Integer.parseInt(tokens[1]),
+             Integer.parseInt(tokens[2]), Integer.parseInt(tokens[3]));
         }
         for (int  i = 0; i < n; i++) {
             int min = i;
