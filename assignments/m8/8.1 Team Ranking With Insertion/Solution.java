238445734c4204621d65e6b9e4734adf4c7edfff
nikhitha goli 20186105 <nikhithagolinikki86@msitprogram.net>
2018-10-02 15:55:15 +0530

    "check_style_score -> 6 of 6 passed. style score: 0/1 "

--- assignments/m8/8.1 Team Ranking With Insertion/Solution.java
+++ assignments/m8/8.1 Team Ranking With Insertion/Solution.java
@@ -27,11 +27,11 @@ class Details {
      * @param      lost   The lost
      * @param      draws  The draws
      */
-    Details(String name, int wins, int lost, int draws) {
-        this.name = name;
-        this.wins = wins;
-        this.lost = lost;
-        this.draws = draws;
+    Details(final String n, final int w, final int l, final int d) {
+        this.name = n;
+        this.wins = w;
+        this.lost = l;
+        this.draws = d;
     }
     /**
      * returns name.
@@ -69,7 +69,7 @@ class Details {
 /**
  * Class for solution.
  */
-class Solution {
+final class Solution {
     /**
      * Constructs the object.
      */
@@ -81,14 +81,14 @@ class Solution {
      *
      * @param      args  The arguments
      */
-    public static void main(String[] args) {
+    public static void main(final String[] args) {
         Scanner sc = new Scanner(System.in);
-        Details[] list = new Details[10];
+        Details[] list = new Details[2*(2+2+1)];
         int n = 0;
         while (sc.hasNextLine()) {
             String[] tokens = sc.nextLine().split(",");
             list[n++] = new Details(tokens[0], Integer.parseInt(tokens[1]),
-             Integer.parseInt(tokens[2]), Integer.parseInt(tokens[3]));
+             Integer.parseInt(tokens[2]), Integer.parseInt(tokens[2+1]));
         }
         for (int  i = 0; i < n; i++) {
             int min = i;
