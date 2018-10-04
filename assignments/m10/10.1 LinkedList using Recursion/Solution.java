45cfc0535f22b4c47c76a2edfa4e6da7978a6312
nikhitha goli 20186105 <nikhithagolinikki86@msitprogram.net>
2018-10-04 21:44:10 +0530

    "ads1_m10_a1: 3/3 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- assignments/m10/10.1 LinkedList using Recursion/Solution.java
+++ assignments/m10/10.1 LinkedList using Recursion/Solution.java
@@ -1,29 +1,74 @@
 import java.util.Scanner;
+/**
+ * Class for node.
+ */
 class Node {
+    /**
+     * node data.
+     */
     String data;
+    /**
+     * next node link.
+     */
     Node next;
-    Node(String value, Node link) {
+    /**
+     * node constructor.
+     *
+     * @param      value  The value
+     * @param      link   The link
+     */
+    Node(final String value, final Node link) {
         this.data = value;
         this.next = link;
     }
 }
-
+/**
+ * Class for linkedlist.
+ */
 class Linkedlist {
+    /**
+     * linkedlist size.
+     */
     private int noOfElements;
+    /**
+     * nodes.
+     */
     private Node head, last, temp;
+    /**
+     * Constructs the object.
+     */
     Linkedlist() {
         noOfElements = 0;
         head = null;
     }
-
-    void insertAt(int position, String value)throws Exception {
+    /**
+     * insert at a position.
+     *
+     * @param      position   The position
+     * @param      value      The value
+     *
+     * @throws     Exception  { exception_description }
+     */
+    void insertAt(final int position, final String value)throws Exception {
         if (position < 0 || position > noOfElements) {
             throw new Exception();
         }
         temp = new Node(value, null);
         head = insertAfter(position, head, temp, 0);
     }
-    Node insertAfter(int position, Node first, Node temp, int count) throws Exception {
+    /**
+     * inserts after a node at a given position.
+     *
+     * @param      position   The position
+     * @param      first      The first
+     * @param      temp       The temporary
+     * @param      count      The count
+     *
+     * @return     { description_of_the_return_value }
+     *
+     * @throws     Exception  { exception_description }
+     */
+    Node insertAfter(final int position, final Node first, final Node temp, final int count) throws Exception {
         if (position == count) {
             temp.next = first;
             noOfElements++;
@@ -32,10 +77,19 @@ class Linkedlist {
         first.next = insertAfter(position, first.next, temp, count + 1);
         return first;
     }
+    /**
+     * to reverse a linked list.
+     */
     void reverse() {
         reverse(null, head);
     }
-    void reverse(Node prev, Node curr) {
+    /**
+     * revers the linkelist.
+     *
+     * @param      prev  The previous
+     * @param      curr  The curr
+     */
+    void reverse(final Node prev, final Node curr) {
         if (curr != null) {
             reverse(curr, curr.next);
             curr.next = prev;
@@ -43,6 +97,11 @@ class Linkedlist {
             head = prev;
         }
     }
+    /**
+     * prints the linkedlist.
+     *
+     * @return     { description_of_the_return_value }
+     */
     String print() {
         temp = head;
         String str = "";
@@ -54,8 +113,22 @@ class Linkedlist {
 
     }
 }
-class Solution {
-    public static void main(String[] args) {
+/**
+ * Class for solution.
+ */
+final class Solution {
+    /**
+     * Constructs the object.
+     */
+    private Solution(){
+        //constructor.
+    }
+    /**
+     * main function.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
         Scanner sc = new Scanner(System.in);
         Linkedlist linkedlist = new Linkedlist();
         while (sc.hasNextLine()) {
