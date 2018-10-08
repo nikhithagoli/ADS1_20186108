8ea152b6da5f1a1ccdf7a7d9b86e7caf87c2509f
nikhitha goli 20186105 <nikhithagolinikki86@msitprogram.net>
2018-10-08 19:05:39 +0530

    "ads1_m13_a1: 5/5 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- assignments/m13/13.1 isMinHeap/Solution.java
+++ assignments/m13/13.1 isMinHeap/Solution.java
@@ -22,47 +22,50 @@ class Solution {
         Minheap m = new Minheap();
         sc.nextLine();
         for (int i = 0; i < nooftestcases; i++) {
-            String[] input = sc.nextLine().split(",");
-            switch (type) {
-            case "String":
-                size  = 0;
-                String[] stringheap = new String[input.length];
-                for (int j = 0; j < input.length; j++) {
-                    stringheap[size++] = input[j];
-                }
-                System.out.println(m.isHeap(stringheap));
-                break;
-            case "Integer":
-                size = 0;
-                Integer[] intheap = new Integer[input.length];
-                for (int j = 0; j < input.length; j++) {
-                    intheap[size++] = Integer.parseInt(input[j]);
-                }
-                System.out.println(m.isHeap(intheap));
-                break;
-            case "Float":
-                if(input.length != 0){
+            String line = sc.nextLine();
+            if(line.length() == 0){
+                System.out.println("false");
+            } else{
+                String[] input = line.split(",");
+                switch (type) {
+                case "String":
+                    size  = 0;
+                    String[] stringheap = new String[input.length];
+                    for (int j = 0; j < input.length; j++) {
+                        stringheap[size++] = input[j];
+                    }
+                    System.out.println(m.isHeap(stringheap));
+                    break;
+                case "Integer":
+                    size = 0;
+                    Integer[] intheap = new Integer[input.length];
+                    for (int j = 0; j < input.length; j++) {
+                        intheap[size++] = Integer.parseInt(input[j]);
+                    }
+                    System.out.println(m.isHeap(intheap));
+                    break;
+                case "Float":
                     size = 0;
                     Float[] floatheap = new Float[input.length];
                     for (int j = 0; j < input.length; j++) {
                         floatheap[size++] = Float.parseFloat(input[j]);
                     }
                     System.out.println(m.isHeap(floatheap));
-                } else {
-                    System.out.println("false");
-                }   
-                break;
-            case "Double":
-                size = 0;
-                Double[] doubleheap = new Double[input.length];
-                for (int j = 0; j < input.length; j++) {
-                    doubleheap[size++] = Double.parseDouble(input[j]);
+                     
+                    break;
+                case "Double":
+                    size = 0;
+                    Double[] doubleheap = new Double[input.length];
+                    for (int j = 0; j < input.length; j++) {
+                        doubleheap[size++] = Double.parseDouble(input[j]);
+                    }
+                    System.out.println(m.isHeap(doubleheap));
+                    break;
+
+
                 }
-                System.out.println(m.isHeap(doubleheap));
-                break;
-
-
             }
+            
         }
     }
 }
