1a547ab8b7cc5c1242ea1c1e3b029470101fdcaa
nikhitha goli 20186105 <nikhithagolinikki86@msitprogram.net>
2018-10-08 11:26:37 +0530

    "ads1_m11_a1_v2: 4/4 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- assignments/m11/11.1 Cutoff Quicksort to Insertion Sort - updated/Solution.java
+++ assignments/m11/11.1 Cutoff Quicksort to Insertion Sort - updated/Solution.java
@@ -1,175 +1,259 @@
 import java.util.Scanner;
-import java.util.Arrays;
-class Quicksort{
-	/**
-     * cutoff to insertion sort.
-     */
-    private int cutoff;
-    /**
-     * Constructs the object.
-     * .
-     */
-    Quicksort() {
-        //Unused Constructor.
-    }
-    /**
-     * swaps the value.
-     *
-     * Best case: O(1)
-     * Average case : O(1)
-     * worst case :O(1)
-     *
-     * @param      array   list
-     * @param      i     {Integer i}
-     * @param      j     {Integer j}
-     */
-    public void swap(final Comparable[] array,
-                     final int i, final int j) {
-        Comparable temp = array[i];
-        array[i] = array[j];
-        array[j] = temp;
-    }
-     /**
-     * checks if a is less than b or not.
-     *
-     * Best case: O(1)
-     * Average case : O(1)
-     * worst case :O(1)
-     *
-     * @param      a     {Comparable}.
-     * @param      b     {Comparable}.
-     *
-     * @return     {Boolean value}.
-     */
-    public boolean less(final Comparable a, final Comparable b) {
-        return a.compareTo(b) < 0;
-    }
-    /**
-     * insertion sorting method.
-     *
-     * Best case: O(N)
-     * Average case : O(N^2/2)
-     * worst case :O(N^2)
-     *
-     * @param      array     array.
-     * @param      low    start
-     * @param      high    high
-     */
-    public void insertionSort(final Comparable[] array,
-                              final int low, final int high) {
-        for (int i = low; i <= high; i++) {
-            for (int j = i; j > low && less(array[j], array[j - 1]); j--) {
-                swap(array, j, j - 1);
-            }
-        }
-    }
-
-    /**
-     * rearranges the array.
-     * 
-     * Best case: O(1)
-     * Average case : O(1)
-     * worst case :O(1)
-     * 
-     * @param      array  The array
-     * @param      m      { parameter_description }
-     */
-    public void sort(final Comparable[] array, int m){
-    	cutoff = m;
-    	sort(array, 0, array.length-1);
-    	System.out.println(Arrays.toString(array));
-    }
-    /**
-     * rearranges the subarrays.
-     * Best case: O(NlogN)
-     * Average case : O(NlogN)
-     * worst case :O(N^2)
-     * 
-     * @param      array  The array
-     * @param      low    The low
-     * @param      high   The high
-     */
-    public void sort(final Comparable[] array, int low, int high){
-    	//System.out.println(low + "			" + high );	
-    	if (high <= low + cutoff - 1) {
-            insertionSort(array, low, high);
-            System.out.println("insertionSort called");
-            //System.out.println(Arrays.toString(array));
-        } else{
-        	int j = partition(array, low, high);
-    		sort(array, low, j-1);
-    		sort(array, j+1, high);
-        }
-    }
-    /**
-     * partition.
-     * 
-     * Best case: O(1)
-     * Average case : O(1)
-     * worst case :O(1)
-     *
-     * @param      array  The array
-     * @param      low    The low
-     * @param      high   The high
-     *
-     * @return     { description_of_the_return_value }
-     */
-    public int partition(final Comparable[] array, final int low, final int high){
-        int i = low, j = high + 1;
-        if(array.length != 0){
-            while(true){
-                while(less(array[++i], array[low])){
-                    if(i == high){
-                        break;
-                    }
-                }
-                while(less(array[low], array[--j])){
-                    if(j == low){
-                        break;
-                    }
-                }
-                if(i >= j){
-                    break;
-                }
-                swap(array, i, j);
-            }
-            swap(array, low, j);
-            System.out.println(Arrays.toString(array));
-        }
-        return j;
-    }
-    
-}
 /**
  * Class for solution.
  */
 public final class Solution {
     /**
-     * Constructs the object.
-     *
+     * checkstyle purpose Constructs the object.
      */
     private Solution() {
-        //Unused Constructor.
+
     }
     /**
-     * main.
+     * main maethod starts here.
      *
      * @param      args  The arguments
      */
     public static void main(final String[] args) {
-        Scanner sc = new Scanner(System.in);
-     	Quicksort quick = new Quicksort();
-     	int noOfInputs = sc.nextInt();
-     	sc.nextLine();
-        for(int i = 0; i < noOfInputs; i++){
-        	int m = sc.nextInt();
-        	sc.nextLine();
-            String[] input = sc.nextLine().split(" ");
-            if(input.length == 0){
-                System.out.println("[]");
-            } else{
-                quick.sort(input, m);
+        Scanner input = new Scanner(System.in);
+        int noOfTestcases = input.nextInt();
+        // System.out.println();
+        for (int i = 0; i < noOfTestcases; i++) {
+            int pivot = input.nextInt();
+            input.nextLine();
+            String elements = input.nextLine();
+            Quick array = new Quick();
+            String[] elearr = elements.split(" ");
+            array.sort(elearr, pivot);
+            // System.out.println(array.show(elearr));
+            array.show(elearr);
+            System.out.println();
+
+
+
+        }
+    }
+}
+/**
+ * Class for quick.
+ */
+class Quick {
+
+    /**
+     * Constructs the object.
+     */
+    protected Quick() {
+        /**
+         * { item_description }
+         */
+    }
+
+    /**
+     * sort function.
+     *
+     * @param      a      { parameter_description }
+     * @param      pivot  The pivot
+     */
+    public static void sort(final Comparable[] a, final int pivot) {
+        sort(a, 0, a.length - 1, pivot);
+        assert isSorted(a);
+    }
+
+    /**
+     * sort function.
+     *
+     * @param      a      { parameter_description }
+     * @param      lo     The lower
+     * @param      hi     The higher
+     * @param      pivot  The pivot
+     */
+    private static void sort(
+        final Comparable[] a, final int lo, final int hi, final int pivot) {
+        if (hi <= lo + pivot - 1) {
+            System.out.println("insertionSort called");
+            insertionSort(a, lo, hi);
+            return;
+        }
+
+        if (hi <= lo) {
+            return;
+        }
+        int j = partition(a, lo, hi);
+        sort(a, lo, j - 1, pivot);
+        sort(a, j + 1, hi, pivot);
+        assert isSorted(a, lo, hi);
+    }
+    /**
+     * partition function.
+     *
+     * @param      a     { parameter_description }
+     * @param      lo    The lower
+     * @param      hi    The higher
+     *
+     * @return     { description_of_the_return_value }
+     */
+    private static int partition(
+        final Comparable[] a, final int lo, final int hi) {
+        int i = lo;
+        int j = hi + 1;
+        Comparable v = a[lo];
+        while (true) {
+
+            // find item on lo to swap
+            while (less(a[++i], v)) {
+                if (i == hi) {
+                    break;
+                }
+            }
+
+            // find item on hi to swap
+            while (less(v, a[--j])) {
+                if (j == lo) {
+
+                    break;
+                }
+            }
+
+
+            // check if pointers cross
+            if (i >= j) {
+                break;
+            }
+
+            exch(a, i, j);
+        }
+
+        // put partitioning item v at a[j]
+        exch(a, lo, j);
+        show(a);
+        System.out.println();
+
+        // now, a[lo .. j-1] <= a[j] <= a[j+1 .. hi]
+        return j;
+    }
+
+    /**.
+     *
+     * @param  a the array
+     * @param  k the rank of the key
+     * @return the key of rank {@code k}
+     * @throws IllegalArgumentException unless {@code 0 <= k < a.length}
+     */
+    public static Comparable select(final Comparable[] a, final int k) {
+        if (k < 0 || k >= a.length) {
+            throw new IllegalArgumentException(
+                "index is not between 0 and " + a.length + ": " + k);
+        }
+        // StdRandom.shuffle(a);
+        int lo = 0, hi = a.length - 1;
+        while (hi > lo) {
+
+            int i = partition(a, lo, hi);
+            if (i > k) {
+                hi = i - 1;
+            } else if (i < k) {
+                lo = i + 1;
+            } else {
+                return a[i];
+            }
+        }
+        return a[lo];
+    }
+
+
+
+    /*********************************************************************
+     *  Helper sorting functions.
+     **********************************************************************/
+    /**
+     * less function.
+     *
+     * @param      v     { parameter_description }
+     * @param      w     { parameter_description }
+     *
+     * @return     { description_of_the_return_value }
+     */
+    private static boolean less(final Comparable v, final Comparable w) {
+        if (v == w) {
+            return false;   // optimization when reference equals
+
+        }
+        return v.compareTo(w) < 0;
+    }
+
+    /**
+     * exchanges the elements.
+     *
+     * @param      a     { parameter_description }
+     * @param      i     { parameter_description }
+     * @param      j     { parameter_description }
+     */
+    private static void exch(final Object[] a, final int i, final int j) {
+        Object swap = a[i];
+        a[i] = a[j];
+        a[j] = swap;
+    }
+
+
+    /**
+     * isorted function.
+     *
+     * @param      a     { parameter_description }
+     *
+     * @return     True if sorted, False otherwise.
+     */
+    private static boolean isSorted(final Comparable[] a) {
+        return isSorted(a, 0, a.length - 1);
+    }
+    /**
+     * Determines if sorted.
+     *
+     * @param      a     { parameter_description }
+     * @param      lo    The lower
+     * @param      hi    The higher
+     *
+     * @return     True if sorted, False otherwise.
+     */
+    private static boolean isSorted(
+        final Comparable[] a, final int lo, final int hi) {
+        for (int i = lo + 1; i <= hi; i++) {
+            if (less(a[i], a[i - 1])) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+
+
+    /**
+     * show the array.
+     *
+     * @param      a     { parameter_description }
+     */
+    public  static void show(final Comparable[] a) {
+        String str = "[";
+        for (int i = 0; i < a.length; i++) {
+            str += a[i] + ", ";
+        }
+        System.out.print(str.substring(0, str.length() - 2) + "]");
+    }
+    /**
+     * {Method for insertion sort}.
+     * sort from a[lo] to a[hi].
+     * Time complexity of insertion sort is O(N^2/2).
+     * @param      a     {Comparable array}.
+     * @param      lo    The lower value
+     * @param      hi    The higher value
+     */
+    public static void insertionSort(final Comparable[] a,
+                                     final int lo, final int hi) {
+        for (int i = lo; i <= hi; i++) {
+            for (int j = i; j > lo && less(a[j], a[j - 1]); j--) {
+                exch(a, j, j - 1);
+                // show(a);
             }
         }
     }
+
 }
