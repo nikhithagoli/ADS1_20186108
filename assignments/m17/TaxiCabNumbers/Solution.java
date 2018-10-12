c64048cca6915c6ccadd9a99cb6077aab670062a
nikhitha goli 20186105 <nikhithagolinikki86@msitprogram.net>
2018-10-12 15:59:12 +0530

    "taxi_cab_numbers: 5/5 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- assignments/m17/TaxiCabNumbers/Solution.java
+++ assignments/m17/TaxiCabNumbers/Solution.java
@@ -443,7 +443,7 @@ final class Solution {
      */
     public static void main(final String[] args) {
 
-        final int num = 523;
+        final int num = 525;
         Scanner sc = new Scanner(System.in);
         ArrayList<CubeSum> cubelist = new ArrayList<CubeSum>();
         // initialize priority queue
