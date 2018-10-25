dc06381def24cb9773a3f6a84eddf4656b0932de
nikhitha goli 20186105 <nikhithagolinikki86@msitprogram.net>
2018-10-25 16:41:52 +0530

    "ads1_m22_a1: 6/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- assignments/m22/22.1 Ransom Note/Solution.java
+++ assignments/m22/22.1 Ransom Note/Solution.java
@@ -1,38 +1,99 @@
 import java.util.Scanner;
 import java.util.Arrays;
+/**
+ * Class for hash table.
+ */
 class HashTable {
+	/**
+	 * Class for node.
+	 */
 	class Node {
+		/**
+		 * word as key.
+		 */
 		private String key;
+		/**
+		 * count as value.
+		 */
 		private Integer val;
+		/**
+		 * next node.
+		 */
 		private Node next;
-		Node(String k, Integer v, Node n) {
+		/**
+		 * Constructs the object.
+		 *
+		 * @param      k     { parameter_description }
+		 * @param      v     { parameter_description }
+		 * @param      n     { parameter_description }
+		 */
+		Node(final String k, final Integer v, final Node n) {
 			this.key = k;
 			this.val = v;
 			this.next = n;
 		}
+		/**
+		 * retunrs key.
+		 *
+		 * @return     { description_of_the_return_value }
+		 */
 		String getkey() {
 			return this.key;
 		}
+		/**
+		 * Gets the value.
+		 *
+		 * @return     The value.
+		 */
 		Integer getValue() {
 			return this.val;
 		}
-		void setvalue(Integer v) {
+		/**
+		 * sets value.
+		 *
+		 * @param      v     { parameter_description }
+		 */
+		void setvalue(final Integer v) {
 			this.val = v ;
 		} 
 	}
+	/**
+	 * nodes table.
+	 */
 	private Node[] st;
+	/**
+	 * size.
+	 */
 	private int s = 97;
+	/**
+	 * Constructs the object.
+	 */
 	HashTable() {
 		st = new Node[s];
 	}
-	
-	int hash(String k) {
+	/**
+	 * hash.
+	 *
+	 * @param      k     { parameter_description }
+	 *
+	 * @return     { description_of_the_return_value }
+	 */
+	int hash(final String k) {
 		return (k.hashCode() & 0x7fffffff) % s;
 	}
+	/**
+	 * resizes.
+	 */
 	public void resize() {
         st = Arrays.copyOf(st, 2 * s);
     }
-	public void put(String k, Integer v) {
+    /**
+     * put.
+     *
+     * @param      k     { parameter_description }
+     * @param      v     { parameter_description }
+     */
+	public void put(final String k, final Integer v) {
 		int i = hash(k);
 		for(Node x =st[i]; x!= null; x = x.next) {
 			if(k.equals(x.getkey())) {
@@ -45,8 +106,14 @@ class HashTable {
 		}
 		st[i] = new Node(k, v, st[i]);
 	}
-
-	public boolean get(String k) {
+	/**
+	 * get.
+	 *
+	 * @param      k     { parameter_description }
+	 *
+	 * @return     { description_of_the_return_value }
+	 */
+	public boolean get(final String k) {
 		int i = hash(k);
 		for(Node x =st[i]; x!= null; x = x.next) {
 			if(k.equals(x.getkey())) {
@@ -60,8 +127,16 @@ class HashTable {
 		return false;
 	}
 }
+/**
+ * Solution class.
+ */
 final class Solution {
-	public static void main(String[] args) {
+	/**
+	 * main.
+	 *
+	 * @param      args  The arguments
+	 */
+	public static void main(final String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int m = sc.nextInt();
 		int n = sc.nextInt();
